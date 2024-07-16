e0cd3b3115ed2a5154cc1c3a8fb9e5c5f10384eb
Yunfan Kang <yfkang@illinois.edu>
2024-07-16 14:33:39 +0000

    Add swagger documentation

---
+++ backend_scripts/swagger.js
@@ -0,0 +1,29 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: `API documentation for the i-guide platform ${process.env.SERV_TAG} backend`,
+    },
+    servers: [
+      {
+        url: 'http://backend-dev.i-guide.io:5001',
+      },
+      {
+        url: 'https://backend-dev.i-guide.io:5000',
+      },
+    ],
+  },
+  apis: ['./server_dev.js'], // Path to the API docs
+};
+
+const specs = swaggerJsdoc(options);
+
+export { specs };
+
