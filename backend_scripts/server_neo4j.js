f9b35baed35a033702f013365373f72da6485ac4
Furqan Baig <fbaig@illinois.edu>
2024-09-25 14:56:41 -0500

    map element fixes + user management + documentation endpoint fixes

--- backend_scripts/server_neo4j.js
+++ backend_scripts/server_neo4j.js
@@ -881,8 +881,9 @@ app.post('/api/elements', jwtCorsMiddleware, authenticateJWT, async (req, res) =
     try {
         console.log(resource['resource-type']);
 	console.log(req.user);
-        // Check if the resource type is "oer" and user's role is greater than 10
-        if (resource['resource-type'] === 'oer' && !(req.user.role <= 4)) {
+        // Check if the resource type is "oer" and user have enough permission to add OER
+        if (resource['resource-type'] === 'oer' &&
+	    !(req.user.role <= n4j.Role.UNRESTRICTED_CONTRIBUTOR)) {
             console.log(req.user, " blocked by role")
             return res.status(403).json({ message: 'Forbidden: You do not have permission to submit OER elements.' });
         }else{
@@ -1034,8 +1035,7 @@ app.put('/api/elements/:id', jwtCorsMiddleware, authenticateJWT, async (req, res
 		console.log('This element is owned by the user');
 		// this element is owned by the user sending update request
 		return true;
-	    } else if (req.user.role <=2) {
-		console.log('This update request is from an admin or super-admin');
+	    } else if (req.user.role <= n4j.Role.CONTENT_MODERATOR) {
 		// user sending update request is admin or super admin
 		return true;
 	    }
@@ -1554,7 +1554,7 @@ app.get('/api/documentation', async (req, res) => {
 	  'size': size} = req.query;
 
     try {
-	const documentation = await n4j.getAllDocumentation(from, size);	
+	const documentation = await n4j.getAllDocumentation(from, size);
 	if (documentation.length == 0){
 	    return res.status(404).json({ message: 'No Documentation found' });
 	}
