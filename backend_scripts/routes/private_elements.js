dcbd9ebd1e18e0901d0a56a2d431b82cf952e109
Furqan Baig <fbaig@illinois.edu>
2024-11-12 14:09:30 -0600

    code refactoring: separate modules for documentation, private_elements and users endpoints

---
+++ backend_scripts/routes/private_elements.js
@@ -0,0 +1,143 @@
+import express from 'express';
+import cors from 'cors';
+// local imports
+import * as n4j from '../backend_neo4j.cjs'
+import { jwtCORSOptions, jwtCorsOptions, jwtCorsMiddleware } from '../iguide_cors.js'
+import { authenticateJWT, authorizeRole, generateAccessToken } from '../jwtUtils.js';
+
+const router = express.Router();
+/**
+ * @swagger
+ * /api/private-elements/{id}:
+ *   get:
+ *     summary: Retrieve ONE private element using id.
+ *     tags: ['private-elements']
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The element ID to fetch
+ *     responses:
+ *       200:
+ *         description: JSON Map object for element with given ID
+ *       403:
+ *         description: Insufficient permission to view this element
+ *       404:
+ *         description: No element found with given ID
+ *       500:
+ *         description: Internal server error
+ */
+router.options('/private-elements/:id', jwtCorsMiddleware);
+router.get('/private-elements/:id', jwtCorsMiddleware, authenticateJWT, async (req, res) => {
+
+    const element_id = decodeURIComponent(req.params['id']);
+    const {user_id, user_role} = (() => {
+	if (!req.user || req.user == null || typeof req.user === 'undefined'){
+	    return {user_id:null, user_role:null};
+	}
+	return {user_id:req.user.id, user_role:req.user.role}
+    })();
+
+    // 'http://cilogon.org/serverA/users/48835826'
+    // const {user_id, user_role} = {user_id: '62992f5f-fd30-41d6-bc19-810cbba752e9',
+    // 				  user_role: n4j.Role.TRUSTED_USER};
+    try {
+	const can_view = await n4j.userCanViewElement(element_id, user_id, user_role);
+	if (!can_view){
+	    res.status(403).json({ message: 'Forbidden: You do not have permission to view this element.' });
+	    return;
+	}
+
+	const element = await n4j.getElementByID(element_id, user_id, user_role);
+	if (JSON.stringify(element) === '{}'){
+	    res.status(404).json({ message: 'Element not found' });
+	    return;
+	}
+
+	res.status(200).json(element);
+    } catch (error) {
+	console.error('/api/resources/:id Error querying:', error);
+	res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+/**
+ * @swagger
+ * /api/private-elements:
+ *   get:
+ *     summary: Retrieve private elements for given user ID
+ *     tags: ['private-elements']
+ *     parameters:
+ *       - in: query
+ *         name: user-id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID to get private elements for
+ *       - in: query
+ *         name: sort-by
+ *         required: false
+ *         schema:
+ *           type: string
+ *           enum: [click_count, creation_time, title]
+ *         description: The field to sort the elements by
+ *       - in: query
+ *         name: order
+ *         required: false
+ *         schema:
+ *           type: string
+ *           enum: [asc, desc]
+ *         description: Sort order of returned elements
+ *       - in: query
+ *         name: from
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The offset value for pagination
+ *       - in: query
+ *         name: size
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: List of JSON Map objects for private elements
+ *       404:
+ *         description: No private elements found for given user ID
+ *       500:
+ *         description: Internal server error
+ */
+router.options('/private-elements', cors());
+router.get('/private-elements', cors(), async (req, res) => {
+//router.get('/private-elements', jwtCorsMiddleware, authenticateJWT, async (req, res) => {
+
+    //const contributor_id = decodeURIComponent(req.params['id']);
+    let {'user-id': contributor_id,
+	 'sort-by': sort_by,
+	 'order': order,
+	 'from': from,
+	 'size': size} = req.query;
+
+    try {
+	const elements = await n4j.getElementsByContributor(contributor_id,
+							    from,
+							    size,
+							    sort_by,
+							    order,
+							    true
+							   );
+	const total_count = await n4j.getElementsCountByContributor(contributor_id, true);
+	if (total_count === 0){
+	    res.status(404).json({ message: 'Element not found' });
+	    return;
+	}
+	res.status(200).json({elements:elements, 'total-count': total_count});
+    } catch (error) {
+	console.error('Error querying:', error);
+	res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+export default router;
---
+++ backend_scripts/routes/users.js
@@ -0,0 +1,361 @@
+/****************************************************************************
+ * User/Contributor Endpoints
+ ****************************************************************************/
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+// local imports
+import * as n4j from '../backend_neo4j.cjs'
+import { jwtCORSOptions, jwtCorsOptions, jwtCorsMiddleware } from '../iguide_cors.js'
+import { authenticateJWT, authorizeRole, generateAccessToken } from '../jwtUtils.js';
+
+const router = express.Router();
+
+// Configure storage for avatars
+const avatarStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+	cb(null, avatarDir);
+    },
+    filename: (req, file, cb) => {
+	// It's a good practice to sanitize the original file name
+	const sanitizedFilename = file.originalname.replace(/[^a-z0-9.]/gi, '_').toLowerCase();
+	cb(null, `${Date.now()}-${sanitizedFilename}`);
+    }
+});
+const uploadAvatar = multer({ storage: avatarStorage });
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Return the user document given the id
+ *     tags: ['users']
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: The user document
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Error fetching the user
+ */
+//router.options('/users/:id', cors());
+router.get('/users/:id', cors(), async (req, res) => {
+    const id = decodeURIComponent(req.params.id);
+    try {
+	const response = await n4j.getContributorByID(id);
+	if (response.size == 0){
+	    return res.status(404).json({ message: 'User not found' });
+	}
+	// remove role attribute
+	delete response['role'];
+	res.status(200).json(response);
+    } catch (error) {
+	console.error('Error fetching user:', error);
+	res.status(500).json({ message: 'Error fetching the user' });
+    }
+});
+
+/**
+ * @swagger
+ * /api/users/{id}/role:
+ *   get:
+ *     summary: Return the user role given the id
+ *     tags: ['users']
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: The user role i.e. admin, user
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Error fetching the user
+ */
+router.options('/users/:id/role', cors());
+router.get('/users/:id/role', cors(), async (req, res) => {
+    const id = decodeURIComponent(req.params.id);
+    try {
+	const response = await n4j.getContributorByID(id);
+	if (response.size == 0){
+	    return res.status(404).json({ message: 'User not found' });
+	}
+	let ret = {'role' : response['role']};
+	res.json(ret);
+    } catch (error) {
+	console.error('Error fetching user:', error);
+	res.status(500).json({ message: 'Error fetching the user' });
+    }
+});
+
+/**
+ * @swagger
+ * /api/users/{id}/valid:
+ *   get:
+ *     summary: Check if a user exists given the id
+ *     tags: ['users']
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: True if user exists, false otherwise
+ *       500:
+ *         description: Error checking the user
+ */
+router.options('/users/:id/valid', cors());
+router.get('/users/:id/valid', cors(), async (req, res) => {
+    const id = decodeURIComponent(req.params.id);
+
+    console.log('Check user ...' + id);
+    // [ToDo] Return {true, version_num} OR {false, -1}
+    try {
+	const response = await n4j.checkContributorByID(id);
+	res.json(response);
+    } catch (error) {
+	console.error('Error checking user:', error);
+	res.status(500).json({ message: 'Error checking the user' });
+    }
+});
+
+/**
+ * @swagger
+ * /api/users/avatar:
+ *   post:
+ *     summary: Upload/update an avatar image for the user profile
+ *     tags: ['users']
+ *     consumes:
+ *       - multipart/form-data
+ *     parameters:
+ *       - in: formData
+ *         name: file
+ *         type: file
+ *         description: The avatar file to upload
+ *       - in: formData
+ *         name: id
+ *         type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: Avatar uploaded successfully
+ *       400:
+ *         description: No file uploaded
+ */
+router.options('/users/avatar', jwtCorsMiddleware);
+router.post('/users/avatar', jwtCorsMiddleware, authenticateJWT, uploadAvatar.single('file'), async (req, res) => {
+    // if (!req.file) {
+    // 	return res.status(400).json({ message: 'No file uploaded' });
+    // }
+
+    // const filePath = `https://${process.env.DOMAIN}:${process.env.PORT}/user-uploads/avatars/${req.file.filename}`;
+
+    // res.json({
+    // 	message: 'Avatar uploaded successfully',
+    // 	url: filePath,
+    // });
+
+    try {
+	const body = JSON.parse(JSON.stringify(req.body));
+	const id = body.id;
+	const newAvatarFile = req.file;
+
+	if (!id || !newAvatarFile) {
+	    return res.status(400).json({ message: 'ID and new avatar file are required' });
+	}
+
+	// Update the user's avatar URL with the new file URL
+	const newAvatarUrl = `https://${process.env.DOMAIN}:${process.env.PORT}/user-uploads/avatars/${newAvatarFile.filename}`;
+
+	const {result, oldAvatarUrl} = await n4j.setContributorAvatar(id, newAvatarUrl);
+	if (result == false){
+	    return res.status(404).json({ message: 'User not found' });
+	}
+	if (oldAvatarUrl) {
+	    // Delete the old avatar file
+	    const oldAvatarFilePath = path.join(avatarDir, path.basename(oldAvatarUrl));
+	    if (fs.existsSync(oldAvatarFilePath)) {
+		fs.unlinkSync(oldAvatarFilePath);
+	    }
+	    var ret_message = 'Avatar updated successfully'
+	} else {
+	    var ret_message = 'Avatar uploaded successfully'
+	}
+
+	res.json({
+	    message: ret_message,
+	    url: newAvatarUrl,
+	});
+    } catch (error) {
+	console.error('Error updating avatar:', error);
+	res.status(500).json({ message: 'Internal server error' });
+    }
+
+});
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Add a new user document
+ *     tags: ['users']
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User added successfully
+ *       500:
+ *         description: Internal server error
+ */
+router.options('/users', jwtCorsMiddleware);
+router.post('/users', jwtCorsMiddleware, authenticateJWT, async (req, res) => {
+    const user = req.body;
+    console.log('Adding new user');
+    //console.log(user);
+
+    try {
+	const id = user['id'];
+	const response = await n4j.registerContributor(user);
+	if (response){
+	    res.status(201).json({ message: 'User added successfully', id: id });
+	} else {
+	    res.status(201).json({ message: 'User already exists', id: id });
+	    console.log('User already exists with id: ' + id);
+	}
+	// [ToDo] Add contributor name to OpenSearch
+
+	// const response = await client.index({
+	//     index: 'users',
+	//     id: user.openid,
+	//     body: user,
+	//     refresh:'wait-for'
+	// });
+	//res.status(201).json({ message: 'User added successfully', id: response.body._id });
+    } catch (error) {
+	console.error('Error adding user:', error);
+	res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update the user document
+ *     tags: ['users']
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       500:
+ *         description: Internal server error
+ */
+// Handle OPTIONS requests for both methods
+router.options('/users/:id', (req, res) => {
+    const method = req.header('Access-Control-Request-Method');
+    if (method === 'PUT') {
+        res.header('Access-Control-Allow-Origin', jwtCORSOptions.origin);
+        res.header('Access-Control-Allow-Methods', 'PUT');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Credentials', 'true');
+    } else if (method === 'GET') {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    res.sendStatus(204); // No content
+});
+router.put('/users/:id', jwtCorsMiddleware, authenticateJWT, async (req, res) => {
+    const id = decodeURIComponent(req.params.id);
+    const updates = req.body;
+
+    console.log('Updating user ...');
+
+    try {
+	const response = await n4j.updateContributor(id, updates);
+	if (response) {
+	    res.json({ message: 'User updated successfully', result: response });
+	} else {
+	    console.log('Error updating user');
+	    res.json({ message: 'Error updating user', result: response });
+	}
+    } catch (error) {
+	console.error('Error updating user:', error);
+	res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// /**
+//  * @swagger
+//  * /api/users/{id}:
+//  *   delete:
+//  *     summary: Delete the user document
+//  *     tags: ['users']
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The OpenID of the user
+//  *     responses:
+//  *       200:
+//  *         description: User deleted successfully
+//  *       500:
+//  *         description: Internal server error
+//  */
+// router.delete('/users/:id', async (req, res) => {
+//   const openid = decodeURIComponent(req.params.id);
+
+//   try {
+//   throw Error('Neo4j: Delete user is not implemented');
+//   // const response = await client.delete({
+//   //     index: 'users',
+//   //     id: openid
+//   // });
+
+//   // res.json({ message: 'User deleted successfully', result: response.body.result });
+//   } catch (error) {
+//   console.error('Error deleting user:', error);
+//   res.status(500).json({ message: 'Internal server error' });
+//   }
+// });
+
+export default router;
