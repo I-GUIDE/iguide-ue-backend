cb37c9781f58e5435ea8bda205a024b0b80f2928
Furqan Baig <fbaig@illinois.edu>
2024-11-18 19:24:24 -0600

    typo fix for missing import in users.js

--- backend_scripts/routes/users.js
+++ backend_scripts/routes/users.js
@@ -7,6 +7,7 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 // local imports
+import * as utils from '../utils.js';
 import * as n4j from '../backend_neo4j.js';
 import * as os from '../backend_opensearch.js';
 import { jwtCORSOptions, jwtCorsOptions, jwtCorsMiddleware } from '../iguide_cors.js';
