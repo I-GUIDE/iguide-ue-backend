821c710ba12f779194556c21bab9ca02efa78dd7
Furqan Baig <fbaig@illinois.edu>
2025-02-18 13:40:51 -0600

    Update utils.js to handle Code element type

--- utils.js
+++ utils.js
@@ -12,7 +12,7 @@ export const ElementType = Object.freeze({
     PUBLICATION: "Publication",
     OER: "Oer", // Open Educational Content
     MAP: "Map",
-    CODE: "Repository",
+    CODE: "Code",
     //Documentation: "Documentation",
 });
 
