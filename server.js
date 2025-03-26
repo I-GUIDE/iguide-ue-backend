f716292d4af26c40f99f0a839ef235b4e0d15c21
arunesh-illinois <arunesh2@illinois.edu>
2025-03-26 17:32:07 -0500

    [FIX]: Changes in response for Get all users API

--- server.js
+++ server.js
@@ -61,10 +61,10 @@ app.use(users);
 app.use(elements);
 
 const target_domain = process.env.JWT_TARGET_DOMAIN;
-const SSLOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY),
-    cert: fs.readFileSync(process.env.SSL_CERT)
-};
+// const SSLOptions = {
+//     key: fs.readFileSync(process.env.SSL_KEY),
+//     cert: fs.readFileSync(process.env.SSL_CERT)
+// };
 /****************************************************************************
  * JWT Specific Functions
  ****************************************************************************/
@@ -295,9 +295,9 @@ app.listen(HTTP_PORT, () => {
     console.log(`HTTP server is running on port ${HTTP_PORT}`);
 });
 
-https.createServer(SSLOptions, app).listen(process.env.PORT, () => {
-    console.log(`HTTPS server is running on port ${process.env.PORT}`);
-});
+// https.createServer(SSLOptions, app).listen(process.env.PORT, () => {
+//     console.log(`HTTPS server is running on port ${process.env.PORT}`);
+// });
 
 // Serve Swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
