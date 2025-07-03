a4b7b5224ab7980951260b82dec594460e430c8c
arunesh-illinois <arunesh2@illinois.edu>
2025-07-03 09:35:23 -0500

    [FIX]: Path resolution for SSL Options fixes

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ import elements from './routes/elements.js';
 import {
 	generateOptimizedDomainList,
 } from "./routes/domain_utils.js";
+import path from "path";
 
 const app = express();
 
@@ -64,9 +65,13 @@ app.use(users);
 app.use(elements);
 
 const target_domain = process.env.JWT_TARGET_DOMAIN;
+// To make sure the path found through env is absolute when running the code
+const keyPath = path.resolve(process.env.SSL_KEY)
+const certPath = path.resolve(process.env.SSL_CERT)
+
 const SSLOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY),
-    cert: fs.readFileSync(process.env.SSL_CERT)
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
 };
 /****************************************************************************
  * JWT Specific Functions
